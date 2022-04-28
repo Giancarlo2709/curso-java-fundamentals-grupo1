73154b548db9ebd11670b80aed7db0a51866e187
Giancarlo <giancarlo2709@gmail.com>
2022-04-27 21:53:36 -0500

    for anidados

---
+++ src/main/java/pe/gyarlequej/sesion3/EstructurasRepetitivasAnidadas.java
@@ -0,0 +1,17 @@
+package pe.gyarlequej.sesion3;
+
+public class EstructurasRepetitivasAnidadas {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            /*int j = 1;
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+            }*/
+            for(int j = 1; j <= 5; j++) {
+                System.out.println(j);
+            }
+        }
+    }
+}
