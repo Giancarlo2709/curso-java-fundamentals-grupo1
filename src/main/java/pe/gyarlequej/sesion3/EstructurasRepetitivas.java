51c60b59e9657e746f4bf49f97fea6601a006b8c
Giancarlo <giancarlo2709@gmail.com>
2022-04-27 21:48:03 -0500

    ejemplos break

--- src/main/java/pe/gyarlequej/sesion3/EstructurasRepetitivas.java
+++ src/main/java/pe/gyarlequej/sesion3/EstructurasRepetitivas.java
@@ -40,6 +40,52 @@ public class EstructurasRepetitivas {
             System.out.println(b);
         }
 
+        System.out.println("**************** For EACH****************");
+
+        /* for(TipoDato nombreVariable : collecion) {
+            bloque de sentencias
+        } */
+
+        String[] names = new String[3];
+        names[0] = "Giancarlo";
+        names[1] = "Elvis";
+        names[2] = "Yarlequé";
+
+        for(String name : names) {
+            System.out.println(name);
+        }
+
+        System.out.println(" ********* Break con for *********");
+        for (int y = 1; y <= 5; y++) {
+            System.out.println("Iterator: " + y);
+            if (y == 3) {
+                break;
+            }
+        }
+
+        System.out.println(" ********* Break con while *********");
+        int e = 1;
+        while (e < 6) {
+            System.out.println("Iterator: " + e);
+            if (e == 4) {
+                break;
+            }
+            e++;
+        }
+
+        System.out.println(" ********* Break Characters *********");
+
+        String words = "Its &a #word";
+        char[] characters = words.toCharArray();
+        for(int z = 0; z < characters.length; z++) {
+            System.out.println(characters[z]);
+            int ascii = characters[z];
+            // System.out.println(ascii);
+            if (ascii >= 33 && ascii <= 47) {
+                break;
+            }
+        }
+
 
     }
 
