554eb457353990f8c606d450885822642f6a66f7
Giancarlo <giancarlo2709@gmail.com>
2022-05-11 21:30:25 -0500

    Clases y Objetos - Resuelto rectangulo

--- src/main/java/pe/gyarlequej/sesion4/EjecutaRectangulo.java
+++ src/main/java/pe/gyarlequej/sesion4/EjecutaRectangulo.java
@@ -3,6 +3,16 @@ package pe.gyarlequej.sesion4;
 public class EjecutaRectangulo {
 
     public static void main(String[] args) {
+        Rectangulo rectangulo1 = new Rectangulo(18, 15);
 
+        System.out.println(rectangulo1.calcularArea());
+        System.out.println(rectangulo1.calcularPerimetro());
+
+        Rectangulo rectangulo2 = new Rectangulo();
+        rectangulo2.setAncho(15);
+        rectangulo2.setLargo(10);
+
+        System.out.println(rectangulo2.calcularArea());
+        System.out.println(rectangulo2.calcularPerimetro());
     }
 }
